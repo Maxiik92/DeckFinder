e0962d05f58bcc7d07d1834562916618abf1f5fa
Tomas Max <tomasmax92@gmail.com>
2023-01-29 14:03:23 +0100

    added comment to db controller

--- backend/src/controller/database-controller.ts
+++ backend/src/controller/database-controller.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 import { DatabaseService } from "../service/database/database-service";
 import log from "../logger";
-
+//this controller is for test purposes only...everything that is done beyond this point will be automated
 export class DatabaseController {
   private readonly _router: Router = Router();
 
